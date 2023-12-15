3e3e1375ba299a63325b9665fd2730dd0e412752
Pie31415 <andy999coc@gmail.com>
2023-12-14 20:45:57 -0500

    removed redundant vis func

--- utils.py
+++ utils.py
@@ -14,16 +14,3 @@ def scale_images(images: torch.FloatTensor, image_size: int) -> torch.FloatTenso
         align_corners=False,
     )
     return scaled_images
-
-
-def visualize_all_masks(masks: List[torch.FloatTensor]):
-    total_width = 256 * len(masks)
-    max_height = 256
-
-    new_im = Image.new("L", (total_width, max_height))
-
-    x_offset = 0
-    for im in masks:
-        new_im.paste(im, (x_offset, 0))
-        x_offset += im.size[0]
-    return new_im
